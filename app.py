b3f85d85a2021a5e242a1d2ca6ef519fe9f7712a
Henry <henrymaguire@hotmail.co.uk>
2017-10-22 11:53:25 +0100

    moving code to repo

---
+++ app.py
@@ -0,0 +1,77 @@
+# app.py
+import datetime
+import functools
+import os
+import re
+import urllib
+
+from flask import (Flask, abort, flash, Markup, redirect, render_template,
+                   request, Response, session, url_for)
+from markdown import markdown
+from markdown.extensions.codehilite import CodeHiliteExtension
+from markdown.extensions.extra import ExtraExtension
+from micawber import bootstrap_basic, parse_html
+from micawber.cache import Cache as OEmbedCache
+from peewee import *
+from playhouse.flask_utils import FlaskDB, get_object_or_404, object_list
+from playhouse.sqlite_ext import *
+
+ADMIN_PASSWORD = 'secret'
+APP_DIR = os.path.dirname(os.path.realpath(__file__))
+DATABASE = 'sqliteext:///%s' % os.path.join(APP_DIR, 'blog.db')
+DEBUG = False
+SECRET_KEY = 'shhh, secret!'  # Used by Flask to encrypt session cookie.
+SITE_WIDTH = 800
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+flask_db = FlaskDB(app)
+database = flask_db.database
+
+oembed_providers = bootstrap_basic(OEmbedCache())
+
+
+class Entry(flask_db.Model):
+    title = CharField()
+    slug = CharField(unique=True)# URL-friendly representation of the title.
+    content = TextField() #markdown-formatted entry.
+    # boolean flag indicating whether the entry is published (visible on site)
+    published = BooleanField(index=True)
+    # peewee automatically will create an auto-incrementing primary key for us,
+    # so we don't need to define this explicitly
+    timestamp = DateTimeField(default=datetime.datetime.now, index=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = re.sub('[^\w]+', '-', self.title.lower())
+        ret = super(Entry, self).save(*args, **kwargs)
+
+        # Store search content.
+        self.update_search_index()
+        return ret
+
+    def update_search_index(self):
+
+
+        try:
+            fts_entry = FTSEntry.get(FTSEntry.entry_id == self.id)
+        except FTSEntry.DoesNotExist:
+            fts_entry = FTSEntry(entry_id=self.id)
+            force_insert = True
+        else:
+            force_insert = False
+        fts_entry.content = '\n'.join((self.title, self.content))
+        fts_entry.save(force_insert=force_insert)
+
+
+class FTSEntry(FTSModel):
+    # The search index will be stored using the FTSEntry model class
+    # The primary key of the indexed entry.
+    entry_id = IntegerField()
+    #Search content for the given entry.
+    content = TextField()
+
+    class Meta:
+        database = database
